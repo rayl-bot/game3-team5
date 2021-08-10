ba323d27011ffab6a101977b61d8a212c4384149
AlvaroVazRod <87012253+AlvaroVazRod@users.noreply.github.com>
2021-08-10 20:33:44 +0200

    Update Events.java

--- MasterMind/src/main/java/Grupo5/MasterMind/events/Events.java
+++ MasterMind/src/main/java/Grupo5/MasterMind/events/Events.java
@@ -11,20 +11,25 @@ public class Events {
 	static final int COLORS = 4;// Constante que pone 4 colores
 	static Random r = new Random(); // generamos un numero aleatorio
 	static Color[] colores = new Color[10];
+	public static PictureBox[] bolasolucion = new PictureBox[4];
+	public static PictureBox[] bolas = new PictureBox[4];
+	public static PictureBox[] bolaresul = new PictureBox[4];
+	public static PictureBox[] bolacolores = new PictureBox[4];
 
 	public void llenarLista() {
-        colores[0]=(Color.yellow);
-        colores[1]=(Color.blue);
-        colores[2]=(Color.cyan);
-        colores[3]=(Color.darkGray);
-        colores[4]=(Color.gray);
-        colores[5]=(Color.green);
-        colores[6]=(Color.lightGray);
-        colores[7]=(Color.magenta);
-        colores[8]=(Color.orange);
-        colores[9]=(Color.pink);
-        colores[10]=(Color.red);
+		colores[0] = (Color.yellow);
+		colores[1] = (Color.blue);
+		colores[2] = (Color.cyan);
+		colores[3] = (Color.darkGray);
+		colores[4] = (Color.gray);
+		colores[5] = (Color.green);
+		colores[6] = (Color.lightGray);
+		colores[7] = (Color.magenta);
+		colores[8] = (Color.orange);
+		colores[9] = (Color.pink);
+		colores[10] = (Color.red);
 	}
+
 	public void comoJugar() {
 		JOptionPane.showMessageDialog(null, "Se juega jugando");
 	}
@@ -33,9 +38,32 @@ public class Events {
 		JOptionPane.showMessageDialog(null, "Alvaro Raul Evelyn");
 	}
 
+	public void comprobarAciertos(PictureBox[] bolasolucion, PictureBox[] bolas, PictureBox[] bolaresul) {
+		int cont = 0, cont2=0;
+		for (int i = 0; i < bolas.length; i++) {
+			if (bolasolucion[i].getColor() == bolas[i].getColor()) {
+				bolaresul[cont].setColor(Color.black);
+				cont++;
+			}
+		}
+		cont=0;
+		for (int i = 0; i < bolas.length; i++) {
+			for (int j = 0; j < bolas.length; j++) {
+				if (bolasolucion[i].getColor()==bolas[j].getColor()) {
+					cont2++;
+				}
+				if (bolaresul[cont].getColor()!=Color.black&&cont2>0) {
+					bolaresul[cont].setColor(Color.white);
+				}
+				cont++;
+				
+			}
+		}
+	}
+
 	// funcionn que crea los colores pasandoles por parametros dos listas de
 	// colores, los disponibles y la lista nueva
-	public void crear_colores( PictureBox[] bolacolores) {
+	public void crear_colores(PictureBox[] bolacolores) {
 		int[] coloresNum = new int[bolacolores.length]; // controlador para comparar los random con los enteros
 		int valor = 0;
 		for (int i = 0; i < bolacolores.length; i++) {
@@ -69,7 +97,7 @@ public class Events {
 		int valor = 0;
 		for (int i = 0; i < bolasolucion.length; i++) {
 			valor = r.nextInt(bolasolucion.length + 1);
-			bolasolucion[i]=bolacolores[valor];
+			bolasolucion[i] = bolacolores[valor];
 		}
 	}
 
